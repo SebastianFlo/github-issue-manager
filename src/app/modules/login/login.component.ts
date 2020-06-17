2e92fb52c24b06939f630b33cbd9fc5674bc6768
Sebastian Florian <rebardesigndk@gmail.com>
2020-06-17 10:29:24 +0200

    :wrench: fix(Login): Login error handling

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/external/auth/authentication.service';
-import { first } from 'rxjs/operators';
+import { first, catchError, map, delay, tap } from 'rxjs/operators';
 import { AppState } from 'src/app/data/state';
 import { Store, select } from '@ngrx/store';
 import * as CoreActions from '../../data/core/actions';
 import * as GithubActions from '../../data/github/actions';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { selectToken } from 'src/app/data/core/selectors';
 import { GithubService } from 'src/app/external/github/github.service';
 import { Apollo } from 'apollo-angular';
@@ -19,49 +24,84 @@ import { User } from 'src/app/data/models';
 @Component({
   selector: 'app-login',
   template: `
+    <div class="flex-layout layout-center-v layout-center-h">
+      <mat-card class="login-card">
+        <mat-card-header *ngIf="!user.name">
+          <mat-card-title>Github Token</mat-card-title>
+          <mat-card-subtitle
+            >Enter an existing Github token or create one on
+            <a target="new" href="https://github.com"
+              >Github</a
+            ></mat-card-subtitle
+          >
+        </mat-card-header>
 
-  <div class="flex-layout layout-center-v layout-center-h">
-    <mat-card class="login-card">
-      <mat-card-header *ngIf="!user.name">
-        <mat-card-title>Github Token</mat-card-title>
-        <mat-card-subtitle>Enter an existing Github token or create one on <a target="new" href="https://github.com">Github</a></mat-card-subtitle>
-      </mat-card-header>
+        <mat-card-content class="flex-layout layout-center-h layout-center-v">
+          <div *ngIf="!user.name && !error" class="token-form">
+            <mat-form-field class="input-full-width">
+              <input
+                [formControl]="authToken"
+                class="input-full-width"
+                matInput
+                placeholder="Ex. ff34885a8624460a855540c6592698d2f1812843"
+                value=""
+              />
+            </mat-form-field>
 
-      <mat-card-content class="flex-layout layout-center-h layout-center-v">
-
-        <div *ngIf="!user.name" class="token-form">
-          <mat-form-field class="input-full-width">
-            <input [formControl]="authToken" class="input-full-width" matInput placeholder="Ex. 123456" value="">
-          </mat-form-field>
-
-          <div *ngIf="authToken.errors?.pattern" class="flex-layout layout-center-h text-warning">
-            Token pattern is invalid
-          </div>
-        </div>
-
-        <div *ngIf="user && user.name" class="welcome-form">
-          <div class="flex-layout layout-center-h layout-center-v">
-            <div>
-              <h3>Welcome {{ user?.name }} 😊</h3>
+            <div
+              *ngIf="authToken.errors?.pattern"
+              class="flex-layout layout-center-h text-warning"
+            >
+              Token pattern is invalid
             </div>
           </div>
-        </div>
 
-        <div *ngIf="loading" class="flex-layout layout-center-h layout-center-v">
-          <mat-progress-spinner diameter="50" mode="indeterminate"></mat-progress-spinner>
-        </div>
+          <div *ngIf="error" class="error-form">
+            <div class="flex-layout layout-center-h layout-center-v">
+              <div class="flex-layout layout-center-h text-center">
+                <h3>Something went wrong 🔌</h3>
+                <p>{{ errorMessage }}</p>
+              </div>
+            </div>
+          </div>
 
-      </mat-card-content>
+          <div *ngIf="user && user.name" class="welcome-form">
+            <div class="flex-layout layout-center-h layout-center-v">
+              <div>
+                <h3>Welcome {{ user?.name }} 😊</h3>
+              </div>
+            </div>
+          </div>
 
-      <mat-card-actions>
-        <div *ngIf="!user.name && !loading" class="flex-layout layout-center-h">
-          <button [disabled]="!authToken.dirty || authToken.invalid" mat-button (click)="addToken()">LOGIN</button>
-        </div>
-      </mat-card-actions>
-    </mat-card>
-  </div>
+          <div
+            *ngIf="loading && !error"
+            class="flex-layout layout-center-h layout-center-v"
+          >
+            <mat-progress-spinner
+              diameter="50"
+              mode="indeterminate"
+            ></mat-progress-spinner>
+          </div>
+        </mat-card-content>
 
-<!--
+        <mat-card-actions>
+          <div
+            *ngIf="!user.name && !loading"
+            class="flex-layout layout-center-h"
+          >
+            <button
+              [disabled]="!authToken.dirty || authToken.invalid"
+              mat-button
+              (click)="addToken()"
+            >
+              LOGIN
+            </button>
+          </div>
+        </mat-card-actions>
+      </mat-card>
+    </div>
+
+    <!--
     <h2>Set Token</h2>
     {{ token$ | async }}
     <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
@@ -100,18 +140,30 @@ import { User } from 'src/app/data/models';
   `,
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   // token$: Observable<string>;
   // reposWithIssues$: Observable<Repo[]>;
 
   authToken = new FormControl('');
   success = true;
   // submitted = false;
+  loginSubscription: Subscription;
   loading = false;
-  // error;
+  error = false;
+  errorMessage = '';
   user: User = { name: '', login: '' };
   // repos: Repos = { edges: [] };
   // returnUrl: string;
+
+
+  LoginQuery = gql`
+    {
+      viewer {
+        login
+        name
+      }
+    }
+  `;
 
   constructor(
     // private formBuilder: FormBuilder,
@@ -119,9 +171,8 @@ export class LoginComponent implements OnInit {
     private router: Router,
     // private authenticationService: AuthenticationService,
     private store: Store<AppState>,
-    private apollo: Apollo,
-    // private githubService: GithubService
-  ) // private alertService: AlertService
+    private apollo: Apollo // private alertService: AlertService
+  ) // private githubService: GithubService
   {
     // redirect to home if already logged in
     // if (this.authenticationService.currentUserValue) {
@@ -132,7 +183,10 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.authToken.setValidators([Validators.required, Validators.pattern('[0-9a-fA-F]{40}')]);
+    this.authToken.setValidators([
+      Validators.required,
+      Validators.pattern('[0-9a-fA-F]{40}'),
+    ]);
   }
 
   addToken() {
@@ -141,97 +195,59 @@ export class LoginComponent implements OnInit {
       CoreActions.SetToken({ payload: this.authToken.value })
     );
 
-    this.apollo
+    this.loginSubscription = this.apollo
+      // .watchQuery({
+      //   query: gql`
+      //     {
+      //       viewer {
+      //         login
+      //         name
+      //       }
+      //     }
+      //   `,
+      // })
       .watchQuery({
-        query: gql`
-          {
-            viewer {
-              login,
-              name
-            }
-          }
-        `
+        query: this.LoginQuery
       })
-      .valueChanges.subscribe((result: any) => {
-        this.user = result.data && result.data.viewer;
-        this.store.dispatch(CoreActions.SetUser({ payload: this.user }));
-
-        setTimeout(() => {
+      .valueChanges
+      .pipe(
+        map(({ data }: { data: any}) => {
+          this.user = data && data.viewer;
+          this.store.dispatch(CoreActions.SetUser({ payload: this.user }));
+          // setTimeout(() => {
+          //   this.router.navigate(['dashboard']);
+          // }, 2 * 1e3);
+        }),
+        catchError((err) => {
+          this.error = true
+          this.errorMessage = err.message;
+          throw 'error in login. Details: ' + err;
+        }),
+        delay(2 * 1E3),
+        tap(() => {
           this.router.navigate(['dashboard']);
-        }, 2 * 1E3)
-      });
+        })
+      ).subscribe();
+
+
+      // .subscribe((result: any) => {
+      //   if(!result.data) {
+      //     this.error = true;
+      //     this.errorMessage = result.message;
+      //     return;
+      //   }
+
+      //   this.user = result.data && result.data.viewer;
+      //   this.store.dispatch(CoreActions.SetUser({ payload: this.user }));
+
+      //   setTimeout(() => {
+      //     this.router.navigate(['dashboard']);
+      //   }, 2 * 1e3);
+      // })
+
   }
 
-  // convenience getter for easy access to form fields
-  // get f() {
-  //   return this.loginForm.controls;
-  // }
-
-  // getRepos() {
-  //   // this.githubService.getRepos();
-  //   this.apollo
-  //     .watchQuery({
-  //       query: gql`
-  //         {
-  //           viewer {
-  //             login
-  //             repositories(last: 10) {
-  //               edges {
-  //                 node {
-  //                   name
-  //                   issues(last: 10) {
-  //                     edges {
-  //                       node {
-  //                         title
-  //                       }
-  //                     }
-  //                   }
-  //                 }
-  //               }
-  //             }
-  //           }
-  //         }
-  //       `,
-  //     })
-  //     .valueChanges.subscribe((result: any) => {
-  //       this.user = result.data && result.data.viewer.login;
-  //       this.repos = result.data && result.data.viewer.repositories;
-  //       // this.loading = result.loading;
-  //       // this.error = result.errors;
-
-  //       this.store.dispatch(CoreActions.SetUser({ payload: this.user }));
-  //       this.store.dispatch(GithubActions.SetRepos({ payload: this.repos }));
-  //     });
-  // }
-
-  // onSubmit() {
-  //   // this.submitted = true;
-
-  //   this.store.dispatch(
-  //     CoreActions.SetToken({ payload: this.f.accessToken.value })
-  //   );
-
-  //   // // reset alerts on submit
-  //   // // this.alertService.clear();
-
-  //   // // stop here if form is invalid
-  //   // if (this.loginForm.invalid) {
-  //   //   return;
-  //   // }
-
-  //   // this.loading = true;
-  //   // this.authenticationService
-  //   //   .login(this.f.accessToken.value)
-  //   //   .pipe(first())
-  //   //   .subscribe(
-  //   //     (data) => {
-  //   //       console.log(data);
-  //   //       // this.router.navigate([this.returnUrl]);
-  //   //     },
-  //   //     (error) => {
-  //   //       // this.alertService.error(error);
-  //   //       this.loading = false;
-  //   //     }
-  //   //   );
-  // }
+  ngOnDestroy() {
+    this.loginSubscription.unsubscribe();
+  }
 }
